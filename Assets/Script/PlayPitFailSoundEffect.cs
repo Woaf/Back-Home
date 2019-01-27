7706891280dc44d908c13f831be757aeb110c678
Woaf <bfazekas@inf.elte.hu>
2019-01-27 19:19:13 +0100

    final result2

---
+++ Assets/Script/PlayPitFailSoundEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayPitFailSoundEffect : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> failSounds;
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(failSounds[UnityEngine.Random.Range(0, failSounds.Count)], Camera.main.transform.position);
+    }
+}
